f77fd413c7794775887e2966b71af6213a75f05e
miguel <miguelpayet@gmail.com>
2019-07-04 20:51:36 -0500

    fecha cambiado

--- app/models/entry.py
+++ app/models/entry.py
@@ -1,35 +1,37 @@
-from .tag import Tag
 from django.db import models
-from django.db.models import Max, Min
+from django.db.models import Max
+from django.db.models import Min
+
+from .tag import Tag
 
 
 class Entry(models.Model):
-    cambiado = models.DateTimeField()
-    creado = models.DateTimeField()
+    cambiado = models.DateTimeField(editable=False)
+    creado = models.DateTimeField(editable=False)
     descripcion = models.TextField()
     handle = models.CharField(max_length=255)
     identry = models.AutoField(primary_key=True)
     titulo = models.CharField(max_length=512)
-    
+
     class Meta:
         db_table = 'entry'
         managed = False
         verbose_name_plural = "entries"
-    
+
     def __str__(self):
         return "%s" % self.titulo
-    
+
     def __unicode__(self):
         return u'%s' % self.titulo
-    
+
     def tags(self):
         tags = Tag.objects.filter(identry=self.identry)
         return list(tags)
-    
+
     @staticmethod
     def ultimo():
         return Entry.objects.latest('creado')
-    
+
     @staticmethod
     def anterior(entry):
         qs = Entry.objects.filter(creado__lt=entry.creado)
@@ -40,7 +42,7 @@ class Entry(models.Model):
             latest = Entry.objects.filter(creado=res['creado__max']).order_by("-identry")
             anterior = latest.first()
         return anterior
-    
+
     @staticmethod
     def siguiente(entry):
         qs = Entry.objects.filter(creado__gt=entry.creado)
@@ -51,7 +53,7 @@ class Entry(models.Model):
         else:
             siguiente = None
         return siguiente
-    
+
     @staticmethod
     def obtener_slug(slug):
         qs = Entry.objects.filter(handle=slug)
